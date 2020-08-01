a8ddc7ed14ae8dfadb12883602df5dbdbade83cb
Rodrigo Nunes <rodrigo_nunes06@hotmail.com>
2020-08-01 16:09:58 -0400

    navegacao pessoa

---
+++ resources/views/pessoa/create.blade.php
@@ -0,0 +1,11 @@
+<h1> formulario de pessoa: </h1>
+<form action="/pessoa" method="POST">
+    <input type="text" name="nome">
+    <input type="text" name="telefone">
+    <input type="text" name="email" >
+    <button type="submit">Salvar</button>
+</form>
+
+<br>
+
+<a href="/pessoas">Voltar</a>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('pessoas', 'PessoaController@index');
+
+Route::get('pessoas/create', 'PessoaController@create');
